46c7fa6464cabb0ebf3aacaf5675f3552f6ea5ce
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-08 01:41:03 +0200

    masterclass exercieses

---
+++ Python_Masterclass_Udemy/01_basic/05_ifWithStrings/ifWithStrings.py
@@ -0,0 +1,8 @@
+tree1 = 'put your first tree name here'
+tree2 = 'put your second tree name here'
+ 
+# add the code to compare the trees
+if tree1 == tree2:
+    print("The trees are the same")
+else:
+    print("The trees are different")
---
+++ Python_Masterclass_Udemy/01_basic/09_for_loop_with_step/for_loop_with_step.py
@@ -0,0 +1,7 @@
+# This solution uses a step value for the range function
+for i in range(0, 101, 7):
+    print(i)
+
+# This solution uses a slice
+for i in range(101)[::7]:
+    print(i)
