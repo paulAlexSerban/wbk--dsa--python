422fac5bc3cb0b709bd3e850e66cc6f34d9f49f0
paulAlexSerban <paul.alex.serban@gmail.com>
2026-05-31 16:02:12 +0300

    feat: restructure

--- 02_alrogithms/01_numbers/celsius_to_fahrenheit/celsius_to_fahrenheit.py
+++
@@ -1,8 +0,0 @@
-def celsius_to_fahrenheit(celsius):
-    """
-    Convert Celsius to Fahrenheit.
-    
-    :param celsius: Temperature in Celsius
-    :return: Temperature in Fahrenheit
-    """
-    return (celsius * 9/5) + 32
